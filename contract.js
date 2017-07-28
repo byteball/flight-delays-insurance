e45596f0b487beacc9973435e64e954a1e269c7b
tony <tonyofbyteball@users.noreply.github.com>
2017-07-28 20:50:58 +0300

    indent

--- contract.js
+++ contract.js
@@ -16,50 +16,55 @@ function getMyAddressFromContract(shared_address, cb) {
 exports.getMyAddressFromContract = getMyAddressFromContract;
 
 exports.checkAndRefundContractsTimeout = () => {
-	db.query("SELECT shared_address, peer_amount FROM data_feeds, contracts WHERE unit IN (\n\
-		SELECT unit_authors.unit FROM unit_authors JOIN units USING(unit)\n\
-		WHERE address = ? \n\
-		AND units.is_stable = 1\n\
-		ORDER BY unit_authors.rowid DESC LIMIT 0,1\n\
+	db.query(
+		"SELECT shared_address, peer_amount FROM data_feeds, contracts \n\
+		WHERE unit IN (\n\
+			SELECT unit_authors.unit FROM unit_authors JOIN units USING(unit)\n\
+			WHERE address = ? \n\
+			AND units.is_stable = 1\n\
+			ORDER BY unit_authors.rowid DESC LIMIT 0,1\n\
 		)\n\
 		AND data_feeds.feed_name = 'timestamp'\n\
 		AND contracts.checked_timeout = 0\n\
-		AND contracts.timeout < data_feeds.int_value", [conf.TIMESTAMPER_ADDRESS], (rows) => {
-		console.error(new Error(rows.length));
-		if (!rows.length) return;
-		let arrAddressesToRefund = [];
-		let arrFullyFundedAddresses = [];
+		AND contracts.timeout < data_feeds.int_value", 
+		[conf.TIMESTAMPER_ADDRESS], 
+		rows => {
+			console.error(new Error(rows.length));
+			if (!rows.length) return;
+			let arrAddressesToRefund = [];
+			let arrFullyFundedAddresses = [];
 
-		async.each(rows, (row, callback) => {
-			db.query("SELECT address, amount FROM outputs JOIN units USING(unit) \n\
-				WHERE address = ? AND amount = ? AND is_stable = 1 AND sequence = 'good'", [row.shared_address, row.peer_amount], (rows2) => {
-				if (rows2.length) {
-					arrFullyFundedAddresses.push(row.shared_address);
-				} else {
-					arrAddressesToRefund.push(row.shared_address);
-				}
-				callback();
-			});
-		}, () => {
-			if (arrFullyFundedAddresses.length)
-				db.query("UPDATE contracts SET checked_timeout = 1, refunded = 0 WHERE shared_address IN (?)", [arrFullyFundedAddresses]);
+			async.each(rows, (row, callback) => {
+				db.query("SELECT address, amount FROM outputs JOIN units USING(unit) \n\
+					WHERE address = ? AND amount = ? AND is_stable = 1 AND sequence = 'good'", [row.shared_address, row.peer_amount], (rows2) => {
+					if (rows2.length) {
+						arrFullyFundedAddresses.push(row.shared_address);
+					} else {
+						arrAddressesToRefund.push(row.shared_address);
+					}
+					callback();
+				});
+			}, () => {
+				if (arrFullyFundedAddresses.length)
+					db.query("UPDATE contracts SET checked_timeout = 1, refunded = 0 WHERE shared_address IN (?)", [arrFullyFundedAddresses]);
 
-			if (!arrAddressesToRefund.length) return;
-			async.each(arrAddressesToRefund, (address, callback) => {
-				getMyAddressFromContract(address, myAddress => {
-					headlessWallet.sendAllBytesFromAddress(address, myAddress, null, (err) => {
-						if (err) {
-							console.error(new Error(err));
-							arrAddressesToRefund.splice(arrAddressesToRefund.indexOf(address), 1);
-						}else {
-							db.query("UPDATE contracts SET checked_timeout = 1, refunded = 1 WHERE shared_address =?", [address]);
-						}
-						callback();
+				if (!arrAddressesToRefund.length) return;
+				async.each(arrAddressesToRefund, (address, callback) => {
+					getMyAddressFromContract(address, myAddress => {
+						headlessWallet.sendAllBytesFromAddress(address, myAddress, null, (err) => {
+							if (err) {
+								console.error(new Error(err));
+								arrAddressesToRefund.splice(arrAddressesToRefund.indexOf(address), 1);
+							}else {
+								db.query("UPDATE contracts SET checked_timeout = 1, refunded = 1 WHERE shared_address =?", [address]);
+							}
+							callback();
+						});
 					});
 				});
 			});
-		});
-	});
+		}
+	);
 };
 
 exports.getListOfContactsForVerification = (cb) => {
