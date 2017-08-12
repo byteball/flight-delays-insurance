b41b1917aaf8deaba0fc0adaed22e7a175c0f127
tony <tonyofbyteball@users.noreply.github.com>
2017-08-12 21:15:30 +0300

    airline

--- insurance.js
+++ insurance.js
@@ -269,12 +269,13 @@ eventBus.on('text', (from_address, text) => {
 			let flight_number = flight.split(' ')[0];
 			let flight_date = flight.split(' ')[1];
 			let arrFlightMatches = flight_number.match(/\b([A-Z0-9]{2})\s*(\d{1,4}[A-Z]?)\b/);
+			let airline = arrFlightMatches[1];
 
 			if (flight && moment(flight_date, "DD.MM.YYYY").isValid()) {
 				let minDay = moment().set("hours", 0).set("minutes", 0).set("seconds", 0).set('milliseconds', 0).add(conf.minDaysBeforeFlight, 'days').valueOf();
 				if (moment(flight_date, "DD.MM.YYYY").valueOf() >= minDay) {
 					if (moment(flight_date, "DD.MM.YYYY").valueOf() <= moment().add(conf.maxMonthsBeforeFlight, 'month').valueOf()) {
-						if (conf.nonInsurableAirlines.indexOf(arrFlightMatches[1]) === -1 && conf.nonInsurableFlights.indexOf(flight_number === -1) {
+						if (conf.nonInsurableAirlines.indexOf(airline) === -1 && conf.nonInsurableFlights.indexOf(flight_number === -1) {
 							state.flight = flight;
 							state.price = null;
 						} else {
