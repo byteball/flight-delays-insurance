b0c7a51e2a9bf5efddcac864a3cc293ee9f7877e
Zaur_Lumanov <zaur18@live.ru>
2018-01-13 21:00:48 +0300

    update

--- calculatePrice.js
+++ calculatePrice.js
@@ -5,162 +5,174 @@ const notifications = require('./notifications');
 const db = require('byteballcore/db');
 const moment = require('moment');
 const conf = require('byteballcore/conf');
+const { checkCriticalWeather } = require('./weather');
 
 function getCountDelayedFlights(objRatings, delay) {
-	let delayedFlights = 0;
+    let delayedFlights = 0;
 
-	if (delay < 15)
-		delayedFlights += objRatings.ontime;
+    if (delay < 15)
+        delayedFlights += objRatings.ontime;
 
-	if (delay < 30)
-		delayedFlights += objRatings.late15;
+    if (delay < 30)
+        delayedFlights += objRatings.late15;
 
-	if (delay < 45)
-		delayedFlights += objRatings.late30;
+    if (delay < 45)
+        delayedFlights += objRatings.late30;
 
-	if (delay <= 45)
-		delayedFlights += objRatings.late45;
+    if (delay <= 45)
+        delayedFlights += objRatings.late45;
 
-	delayedFlights += objRatings.cancelled;
-	delayedFlights += objRatings.diverted;
-	return delayedFlights;
+    delayedFlights += objRatings.cancelled;
+    delayedFlights += objRatings.diverted;
+    return delayedFlights;
 }
 
 function getRatings(flight, cb) {
-	let arrFlightMatches = flight.match(/\b([A-Z0-9]{2})\s*(\d{1,4}[A-Z]?)\b/);
-	db.query("SELECT * FROM flightstats_ratings WHERE flight=? AND departure_airport IS NOT NULL AND date > " + db.addTime('-30 days'), [flight], (rows) => {
-		if (rows.length !== 0) {
-			cb(null, rows[0]);
-		} else {
-			request('https://api.flightstats.com/flex/ratings/rest/v1/json/flight/' + arrFlightMatches[1] + '/' + arrFlightMatches[2] +
-				'?appId=' + conf.flightstats.appId + '&appKey=' + conf.flightstats.appKey, (error, response, body) => {
-				if (error || response.statusCode !== 200) {
-					notifications.notifyAdmin("getting flightstats data for failed: " + error + ", status=" + response.statusCode);
-					return cb("Failed to fetch flightstats data.");
-				}
-				console.log(flight+' ratings response: '+body);
-				let jsonResult = JSON.parse(body);
-				if (jsonResult.error && jsonResult.error.errorMessage) {
-					notifications.notifyAdmin("error from flightstats: " + body);
-					return cb("Error from flightstats: " + jsonResult.error.errorMessage);
-				}
+    let arrFlightMatches = flight.match(/\b([A-Z0-9]{2})\s*(\d{1,4}[A-Z]?)\b/);
+    db.query("SELECT * FROM flightstats_ratings WHERE flight=? AND departure_airport IS NOT NULL AND date > " + db.addTime('-30 days'), [flight], (rows) => {
+        if (rows.length !== 0) {
+            cb(null, rows[0]);
+        } else {
+            request('https://api.flightstats.com/flex/ratings/rest/v1/json/flight/' + arrFlightMatches[1] + '/' + arrFlightMatches[2] +
+                '?appId=' + conf.flightstats.appId + '&appKey=' + conf.flightstats.appKey, (error, response, body) => {
+                    if (error || response.statusCode !== 200) {
+                        notifications.notifyAdmin("getting flightstats data for failed: " + error + ", status=" + response.statusCode);
+                        return cb("Failed to fetch flightstats data.");
+                    }
+                    console.log(flight + ' ratings response: ' + body);
+                    let jsonResult = JSON.parse(body);
+                    if (jsonResult.error && jsonResult.error.errorMessage) {
+                        notifications.notifyAdmin("error from flightstats: " + body);
+                        return cb("Error from flightstats: " + jsonResult.error.errorMessage);
+                    }
 
-				if (!Array.isArray(jsonResult.ratings)) return cb("No information about this flight.");
+                    if (!Array.isArray(jsonResult.ratings)) return cb("No information about this flight.");
 
-				let objRatings = chooseBestRating(jsonResult.ratings);
-				objRatings.departure_airport = objRatings.departureAirportFsCode;
-				objRatings.arrival_airport = objRatings.arrivalAirportFsCode;
+                    let objRatings = chooseBestRating(jsonResult.ratings);
+                    objRatings.departure_airport = objRatings.departureAirportFsCode;
+                    objRatings.arrival_airport = objRatings.arrivalAirportFsCode;
 
-				if (objRatings.observations >= conf.minObservations)
-					db.query("INSERT OR REPLACE INTO flightstats_ratings (flight, date, observations, ontime, late15, late30, late45, cancelled, diverted, delayMax, departure_airport, arrival_airport) VALUES(?, " + db.getNow() + ", ?, ?,?,?,?, ?,?, ?, ?,?)",
-						[flight, objRatings.observations, objRatings.ontime, objRatings.late15, objRatings.late30, objRatings.late45, objRatings.cancelled, objRatings.diverted, objRatings.delayMax, objRatings.departure_airport, objRatings.arrival_airport]);
-				else
-					console.log('only '+objRatings.observations+' observations');
+                    if (objRatings.observations >= conf.minObservations)
+                        db.query("INSERT OR REPLACE INTO flightstats_ratings (flight, date, observations, ontime, late15, late30, late45, cancelled, diverted, delayMax, departure_airport, arrival_airport) VALUES(?, " + db.getNow() + ", ?, ?,?,?,?, ?,?, ?, ?,?)", [flight, objRatings.observations, objRatings.ontime, objRatings.late15, objRatings.late30, objRatings.late45, objRatings.cancelled, objRatings.diverted, objRatings.delayMax, objRatings.departure_airport, objRatings.arrival_airport]);
+                    else
+                        console.log('only ' + objRatings.observations + ' observations');
 
-				cb(null, objRatings);
-			});
-		}
-	});
+                    cb(null, objRatings);
+                });
+        }
+    });
 }
 
-function chooseBestRating(arrRatings){
-	if (arrRatings.length === 1)
-		return arrRatings[0];
-	if (arrRatings.length === 0)
-		throw Error('no ratings');
-	var r;
-	var maxObservations = 0;
-	arrRatings.forEach(objRatings => {
-		if (objRatings.observations > maxObservations){
-			maxObservations = objRatings.observations;
-			r = objRatings;
-		}
-	});
-	if (!r)
-		throw Error('no best rating');
-	return r;
+function chooseBestRating(arrRatings) {
+    if (arrRatings.length === 1)
+        return arrRatings[0];
+    if (arrRatings.length === 0)
+        throw Error('no ratings');
+    var r;
+    var maxObservations = 0;
+    arrRatings.forEach(objRatings => {
+        if (objRatings.observations > maxObservations) {
+            maxObservations = objRatings.observations;
+            r = objRatings;
+        }
+    });
+    if (!r)
+        throw Error('no best rating');
+    return r;
 }
 
 function offlineCalculate(state, cb) {
-	let flight = state.flight.split(' ')[0];
-	let arrFlightMatches = flight.match(/\b([A-Z0-9]{2})\s*(\d{1,4}[A-Z]?)\b/);
+    let flight = state.flight.split(' ')[0];
+    let arrFlightMatches = flight.match(/\b([A-Z0-9]{2})\s*(\d{1,4}[A-Z]?)\b/);
 
-	let percent = 0;
-	if (state.delay >= 45) percent = conf.defaultPriceInPercent.gt45;
-	else if (state.delay >= 30) percent = conf.defaultPriceInPercent.gt30;
-	else if (state.delay >= 15) percent = conf.defaultPriceInPercent.gt15;
-	else percent = conf.defaultPriceInPercent.gt0;
+    let percent = 0;
+    if (state.delay >= 45) percent = conf.defaultPriceInPercent.gt45;
+    else if (state.delay >= 30) percent = conf.defaultPriceInPercent.gt30;
+    else if (state.delay >= 15) percent = conf.defaultPriceInPercent.gt15;
+    else percent = conf.defaultPriceInPercent.gt0;
 
-	if (conf.coefficientsForFlight && conf.coefficientsForFlight[flight]) {
-		percent *= conf.coefficientsForFlight[flight];
-	} else if (conf.coefficientForAirline && conf.coefficientForAirline[arrFlightMatches[1]]) {
-		percent *= conf.coefficientForAirline[arrFlightMatches[1]];
-	}
-	if (percent > conf.maxPriceInPercent) {
-		return cb("The probability of this delay is too high, please increase the delay time.");
-	}
+    if (conf.coefficientsForFlight && conf.coefficientsForFlight[flight]) {
+        percent *= conf.coefficientsForFlight[flight];
+    } else if (conf.coefficientForAirline && conf.coefficientForAirline[arrFlightMatches[1]]) {
+        percent *= conf.coefficientForAirline[arrFlightMatches[1]];
+    }
+    if (percent > conf.maxPriceInPercent) {
+        return cb("The probability of this delay is too high, please increase the delay time.");
+    }
 
-	let price = state.compensation * percent / 100;
-	if (price.toString().match(/\./)) {
-		if (price.toString().split('.')[1].length > 9) price = price.toFixed(9);
-	}
-	return cb(null, price);
+    let price = state.compensation * percent / 100;
+    if (price.toString().match(/\./)) {
+        if (price.toString().split('.')[1].length > 9) price = price.toFixed(9);
+    }
+    return cb(null, price);
 }
 
 module.exports = (state, cb) => {
-	let flight = state.flight.split(' ')[0];
+    let flight = state.flight.split(' ')[0];
 
-	state.departure_airport = null;
-	state.arrival_airport = null;
-	
-	if (conf.analysisOfRealTimeDelays) {
-		getRatings(flight, (err, objRatings) => {
-			if (err) return cb(err);
+    state.departure_airport = null;
+    state.arrival_airport = null;
 
-			state.departure_airport = objRatings.departure_airport;
-			state.arrival_airport = objRatings.arrival_airport;
+    if (conf.analysisOfRealTimeDelays) {
+        checkCriticalWeather(state.flight, (criticality) => {
+            if (criticality) {
+                state.flight = null;
+                state.delay = null;
+                state.compensation = null;
+                state.price = null;
+                state.save();
 
-			let minDelay = 0;
-			let maxDelay = 0;
+                return cb('The insurance is refused due to critical weather.')
+            }
 
-			if (state.delay <= 15 || objRatings.delayMax <= 15) {
-				minDelay = 0;
-				maxDelay = 15;
-			} else if (state.delay <= 30 || objRatings.delayMax <= 30) {
-				minDelay = 15;
-				maxDelay = 30;
-			} else if (state.delay <= 45 || objRatings.delayMax <= 45) {
-				minDelay = 30;
-				maxDelay = 45;
-			} else {
-				minDelay = 45;
-				maxDelay = objRatings.delayMax;
-			}
+            getRatings(flight, (err, objRatings) => {
+                if (err) return cb(err);
 
-			let percentageDelays = 100 * getCountDelayedFlights(objRatings, maxDelay) / objRatings.observations;
-			let percentageDelays2 = 100 * getCountDelayedFlights(objRatings, minDelay) / objRatings.observations;
+                state.departure_airport = objRatings.departure_airport;
+                state.arrival_airport = objRatings.arrival_airport;
 
-			let percent;
+                let minDelay = 0;
+                let maxDelay = 0;
 
-			percent = (percentageDelays2 + (percentageDelays - percentageDelays2) * (Math.min(state.delay, maxDelay) - minDelay) / (maxDelay - minDelay)) + conf.profitMargin;
+                if (state.delay <= 15 || objRatings.delayMax <= 15) {
+                    minDelay = 0;
+                    maxDelay = 15;
+                } else if (state.delay <= 30 || objRatings.delayMax <= 30) {
+                    minDelay = 15;
+                    maxDelay = 30;
+                } else if (state.delay <= 45 || objRatings.delayMax <= 45) {
+                    minDelay = 30;
+                    maxDelay = 45;
+                } else {
+                    minDelay = 45;
+                    maxDelay = objRatings.delayMax;
+                }
 
-			if (percent > conf.maxPriceInPercent) {
-				return cb("The probability of this delay is too high, please increase the delay time.");
-			}
-			let price = state.compensation * percent / 100;
-			if (price.toString().match(/\./)) {
-				if (price.toString().split('.')[1].length > 9) price = price.toFixed(9);
-			}
-			if (objRatings.observations < conf.minObservations)
-				offlineCalculate(state, function(err, offline_price){
-					if (err)
-						return cb(err);
-					cb(null, Math.max(price, offline_price));
-				});
-			else
-				cb(null, price);
-		});
-	} else {
-		offlineCalculate(state, cb);
-	}
+                let percentageDelays = 100 * getCountDelayedFlights(objRatings, maxDelay) / objRatings.observations;
+                let percentageDelays2 = 100 * getCountDelayedFlights(objRatings, minDelay) / objRatings.observations;
+
+                let percent;
+
+                percent = (percentageDelays2 + (percentageDelays - percentageDelays2) * (Math.min(state.delay, maxDelay) - minDelay) / (maxDelay - minDelay)) + conf.profitMargin;
+
+                if (percent > conf.maxPriceInPercent) {
+                    return cb("The probability of this delay is too high, please increase the delay time.");
+                }
+                let price = state.compensation * percent / 100;
+                if (price.toString().match(/\./)) {
+                    if (price.toString().split('.')[1].length > 9) price = price.toFixed(9);
+                }
+                if (objRatings.observations < conf.minObservations)
+                    offlineCalculate(state, function(err, offline_price) {
+                        if (err)
+                            return cb(err);
+                        cb(null, Math.max(price, offline_price));
+                    });
+                else
+                    cb(null, price);
+            });
+        });
+    } else {
+        offlineCalculate(state, cb);
+    }
 };
--- weather.js
+++ weather.js
@@ -3,135 +3,135 @@ const md5 = require('md5');
 const moment = require('moment');
 const conf = require('byteballcore/conf');
 const scCache = require('./self-cleaningCache'),
-	cache = new scCache;
+    cache = new scCache;
 
 exports.checkCriticalWeather = (flightText, callback) => {
-	if (!flightText) 
-		return callback();
+    if (!flightText)
+        return callback();
 
-	const [carrier, flight, day, month, year] = flightText.match(/([a-z]+)(\d+)\s*(\d+).(\d+).(\d+)/i).splice(1,5);
-	
-	if (day > 31 || month > 12)
-		return callback(texts.invalidDate());
-	
-	const key = md5([carrier, flight, day, month, year].join('_'))
+    const [carrier, flight, day, month, year] = flightText.match(/([a-z]+)(\d+)\s*(\d+).(\d+).(\d+)/i).splice(1, 5);
 
-	const weather = (airports) => {
-			const 
-				currentDay = moment(`${+year}-${+month}-${+day}`), 
-				lastDay = new Date(currentDay),
-				preDay = new Date(currentDay);
+    if (day > 31 || month > 12)
+        return callback(texts.invalidDate());
 
-			lastDay.setHours(lastDay.getHours()-24);
-			preDay.setHours(preDay.getHours()+24);
+    const key = md5([carrier, flight, day, month, year].join('_'))
 
-			const flightDates = [
-				moment(lastDay).format('YYYY-MM-DD'),
-				currentDay.format('YYYY-MM-DD'),
-				moment(preDay).format('YYYY-MM-DD')
-			];
-			
-			const checkWeatherAtCurrentAirport = (airport) => {
-				return new Promise((resolve, reject) => {
-					const check = dayForecasts => {
-						for (const object of dayForecasts) {
-							if (object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[0]
-								|| object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[1]
-								|| object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[2]) {
-								if (conf.critical.indexOf(object.tags[0].value) != -1) {
-									resolve(false);
-								}
-							}
+    const weather = (airports) => {
+        const
+            currentDay = moment(`${+year}-${+month}-${+day}`),
+            lastDay = new Date(currentDay),
+            preDay = new Date(currentDay);
 
-							resolve(true);
-						}
-					};
+        lastDay.setHours(lastDay.getHours() - 24);
+        preDay.setHours(preDay.getHours() + 24);
 
-					let weather = cache.get(`weather_${airport}_${key}`);
+        const flightDates = [
+            moment(lastDay).format('YYYY-MM-DD'),
+            currentDay.format('YYYY-MM-DD'),
+            moment(preDay).format('YYYY-MM-DD')
+        ];
 
-					if (!weather) {
-						request({
-							url: 'https://api.flightstats.com/flex/weather/rest/v1/json/zf/' + airport,
-							qs: {
-								appId: conf.flightstats.appId,
-								appKey: conf.flightstats.appKey,
-								codeType: 'fs'
-							},
-							json: true
-						}, (error, response, body) => {
-							if (error) 
-								throw error;
+        const checkWeatherAtCurrentAirport = (airport) => {
+            return new Promise((resolve, reject) => {
+                const check = dayForecasts => {
+                    for (const object of dayForecasts) {
+                        if (object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[0] ||
+                            object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[1] ||
+                            object.start.replace(/(\d+)-(\d+)-(\d+).*/, '$1-$2-$3') == flightDate[2]) {
+                            if (conf.critical.indexOf(object.tags[0].value) != -1) {
+                                resolve(false);
+                            }
+                        }
 
-							if (!body.zoneForecast) {
-								cache.set(`weather_${airport}_${key}`, 'none');
+                        resolve(true);
+                    }
+                };
 
-								return callback();
-							}
-							
-							weather = body.zoneForecast.dayForecasts;
+                let weather = cache.get(`weather_${airport}_${key}`);
 
-							cache.set(`weather_${airport}_${key}`, weather);
-							check(weather);
-						});
-					} else if (weather == 'none')
-						return callback();
+                if (!weather) {
+                    request({
+                        url: 'https://api.flightstats.com/flex/weather/rest/v1/json/zf/' + airport,
+                        qs: {
+                            appId: conf.flightstats.appId,
+                            appKey: conf.flightstats.appKey,
+                            codeType: 'fs'
+                        },
+                        json: true
+                    }, (error, response, body) => {
+                        if (error)
+                            throw error;
 
-					check(weather);
-				});
-			};
+                        if (!body.zoneForecast) {
+                            cache.set(`weather_${airport}_${key}`, 'none');
 
-			callback();
+                            return callback();
+                        }
 
-			checkWeatherAtCurrentAirport(airports[0])
-				.then(status => {
-					if (!status) {
-						return callback(texts.criticalWeather());
-					}
+                        weather = body.zoneForecast.dayForecasts;
 
-					return checkWeatherAtCurrentAirport(airports[0]);
-				})
-				.then(check.then(status => {
-					if (!status) {
-						return callback(texts.criticalWeather());
-					}
+                        cache.set(`weather_${airport}_${key}`, weather);
+                        check(weather);
+                    });
+                } else if (weather == 'none')
+                    return callback();
 
-					callback();
-				}).catch(error => {
-					throw error;
-				})).catch(error => {
-					throw error;
-				});
-	}
+                check(weather);
+            });
+        };
 
-	let airports = cache.get(`airport_${key}`);
+        callback();
 
-	if (!airports) {
-		return request({
-			url: 'https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/' + carrier +
-				'/' + flight + '/arr/' + year + '/' + month + '/' + day,
-			qs: {
-				appId: conf.flightstats.appId,
-				appKey: conf.flightstats.appKey,
-				utc: false
-			},
-			json: true
-		}, (error, response, body) => {
-			if (error) 
-				throw error;
+        checkWeatherAtCurrentAirport(airports[0])
+            .then(status => {
+                if (!status) {
+                    return callback(texts.criticalWeather());
+                }
 
-			if (!body.appendix.airports) {
-				cache.set(`airport_${key}`, 'none')
+                return checkWeatherAtCurrentAirport(airports[1]);
+            })
+            .then(check.then(status => {
+                if (!status) {
+                    return callback(texts.criticalWeather());
+                }
 
-				return callback();
-			}
+                callback();
+            }).catch(error => {
+                throw error;
+            })).catch(error => {
+                throw error;
+            });
+    }
 
-			airports = [body.appendix.airports[0].fs, body.appendix.airports[1].fs];
-			cache.set(`airport_${key}`, airports)
+    let airports = cache.get(`airport_${key}`);
 
-			weather(airports);
-		});	
-	} else if (airports == 'none')
-		return callback();
-	
-	weather(airports);
+    if (!airports) {
+        return request({
+            url: 'https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/' + carrier +
+                '/' + flight + '/arr/' + year + '/' + month + '/' + day,
+            qs: {
+                appId: conf.flightstats.appId,
+                appKey: conf.flightstats.appKey,
+                utc: false
+            },
+            json: true
+        }, (error, response, body) => {
+            if (error)
+                throw error;
+
+            if (!body.appendix.airports) {
+                cache.set(`airport_${key}`, 'none')
+
+                return callback();
+            }
+
+            airports = [body.appendix.airports[0].fs, body.appendix.airports[1].fs];
+            cache.set(`airport_${key}`, airports)
+
+            weather(airports);
+        });
+    } else if (airports == 'none')
+        return callback();
+
+    weather(airports);
 }
