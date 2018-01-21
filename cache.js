5d2c97d79ef99ee1ddf2194e241cffa19dc5fc1d
Zaur_Lumanov <zaur18@live.ru>
2018-01-21 20:36:45 +0300

    fixes

--- cache.js
+++ cache.js
@@ -4,32 +4,32 @@ const db = require('byteballcore/db');
 const seconds = 60 * 60
 
 module.exports = class {
-    constructor(name, key, value) {
-        this.name = name
-        this.key = key
-        this.value = value
+	constructor(name, key, value) {
+		this.name = name
+		this.key = key
+		this.value = value
 
-        setInterval(this.clear, 1000*60, 1000, this);
-    }
+		setInterval(this.clear, 1000*60, 1000, this);
+	}
 
-    clear(self) {
-        const ts = new Date
+	clear(self) {
+		const ts = new Date
 
-        ts.setHours(ts.getHours()-conf.cacheHours)
+		ts.setHours(ts.getHours()-conf.cacheHours)
 
-        db.query(`DELETE FROM ${this.name}_cache WHERE ts<=?`, [Math.floor(ts.getTime()/1000)]);
-    }
+		db.query(`DELETE FROM ${this.name}_cache WHERE ts<=?`, [Math.floor(ts.getTime()/1000)]);
+	}
 
-    set(key, data) {
-        db.query(`INSERT OR REPLACE INTO ${this.name}_cache (${this.key}, ${this.value}, ts) VALUES(?, ?, ?)`, 
-        [key, JSON.stringify(data), Math.floor((new Date).getTime()/1000)]);
-    }
+	set(key, data) {
+		db.query(`INSERT OR REPLACE INTO ${this.name}_cache (${this.key}, ${this.value}, ts) VALUES(?, ?, ?)`, 
+		[key, JSON.stringify(data), Math.floor((new Date).getTime()/1000)]);
+	}
 
-    get(key) {
-        return new Promise((resolve, reject) => {
-            db.query(`SELECT ${this.value} FROM ${this.name}_cache WHERE ${this.key}=?`, [key], (rows) => {
-                resolve(rows.length ? JSON.parse(rows[0].value) : null)
-            });
-        });
-    }
+	get(key) {
+		return new Promise((resolve, reject) => {
+			db.query(`SELECT ${this.value} FROM ${this.name}_cache WHERE ${this.key}=?`, [key], (rows) => {
+				resolve(rows.length ? JSON.parse(rows[0].value) : null)
+			});
+		});
+	}
 }
